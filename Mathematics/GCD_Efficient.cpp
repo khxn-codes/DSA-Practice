6aab70abda0cf5fa02658c2064bbd9853ac7545d
khxn-codes <shaxn1206@gmail.com>
2026-08-11 18:47:42 +0530

    Add GCD_Efficient.cpp solution

--- Mathematics/GCD_Efficient.cpp
+++ Mathematics/GCD_Efficient.cpp
@@ -1,2 +1,25 @@
 // By Basic Implementation of Euclidean Algorithm
-
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int gcd(int a,int b)
+{
+  int res=min(a,b);
+  while(res>0)
+  {
+    if(a%res==0 && b%res==0)
+    {
+      break;
+    }
+    res--;
+  }
+  return res;
+}
+int main()
+{
+  int a,b;
+  cout<<"Enter the numbers=";
+  cin>>a>>b;
+  cout<<"The GCD of "<<a<<" and "<<b<<" is "<<gcd(a,b);
+  return 0;
+}
