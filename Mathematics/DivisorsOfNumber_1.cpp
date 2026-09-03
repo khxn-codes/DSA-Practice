f49ab5bd444b703199bae1687b76a567317998af
khxn-codes <shaxn1206@gmail.com>
2026-09-03 14:25:46 +0530

    Efficient Method

---
+++ Mathematics/DivisorsOfNumber_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+void divisors(int n)
+{
+  for(int i=1;i*i<=n;i++)
+  {
+    if(n%i==0)
+    {
+      cout<<i<<" ";
+      if(i!=n/i)
+      {
+        cout<<n/i<<" ";
+      }
+    }
+  }
+}
+int main()
+{
+  int n;
+  cout<<"Enter the Number=";
+  cin>>n;
+  cout<<"The divisors of "<<n<<" are ";
+  divisors(n);
+  return 0;
+}
