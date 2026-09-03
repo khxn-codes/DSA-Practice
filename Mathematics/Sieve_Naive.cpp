c63d747cf8e902b051568983d9687d403a41b5f9
khxn-codes <shaxn1206@gmail.com>
2026-09-03 14:38:30 +0530

    Naive of Sieve

---
+++ Mathematics/Sieve_Naive.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+bool isPrime(int n)
+{
+  if(n<=1)
+  {
+    return false;
+  }
+  if(n==2 || n==3)
+  {
+    return true;
+  }
+  if(n%2==0 || n%3==0)
+  {
+    return false;
+  }
+  for(int i=5;i*i<=n;i=i+6)
+  {
+    if(n%i==0 || n%(i+2)==0)
+    {
+      return false;
+    }
+  }
+  return true;
+}
+void sieve(int n)
+{
+  for(int i=2;i<=n;i++)
+  {
+    if(isPrime(i))
+    {
+      cout<<i<<" ";
+    }
+  }
+}
+int main()
+{
+  int n;
+  cout << "Enter the number = ";
+  cin >> n;
+  cout << "The Prime numbers upto " << n << " are: ";
+  sieve(n);
+  return 0;
+}
